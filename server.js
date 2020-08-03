60e168e76f5230ffddca77db242fa13df8a40188
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-03 19:58:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to this NodeJS application." });
 });
 require("./app/routes/aluno.routes.js")(app);
 

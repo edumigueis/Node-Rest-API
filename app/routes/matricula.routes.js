28eecffd38e6442ec81fa6ac69de1d55a0d6aca1
Manuela Vicente Benassi <manuelavbenassi@gmail.com>
2020-08-03 19:57:47 -0300

    mudanças para rotas do bd e correção de erros

--- app/routes/matricula.routes.js
+++ app/routes/matricula.routes.js
@@ -1,25 +1,21 @@
 module.exports = app => {
-  const customers = require("../controllers/Matricula.controller.js");
+  const matricula = require("../controllers/Matricula.controller.js");
 
-  module.exports = app => {
-    const disciplina = require("../controllers/Disciplina.controller.js");
+  // Cria uma nova matricula
+  app.post("/matricula", matricula.create);
 
-    // Cria uma nova matricula
-    app.post("/matricula", matricula.create);
+  // Busca todas as matriculas
+  app.get("/matricula", matricula.findAll);
 
-    // Busca todas as matriculas
-    app.get("/matricula", matricula.findAll);
+  //Busca uma matricula com id especifico
+  app.get("/matricula/:matriculaRA", matricula.findOne);
 
-    //Busca uma matricula com id especifico
-    app.get("/matricula/:matriculaRA", matricula.findOne);
+  // Altera uma matricula com id especifico
+  app.put("/matricula/:matriculaRA", matricula.update);
 
-    // Altera uma matricula com id especifico
-    app.put("/matricula/:matriculaRA", matricula.update);
+  // Deleta uma matricula com id especifico
+  app.delete("/matricula/:matriculaRA", matricula.delete);
 
-    // Deleta uma matricula com id especifico
-    app.delete("/matricula/:matriculaRA", matricula.delete);
-
-    // Deleta todas as matriculas
-    app.delete("/matricula", matricula.deleteAll);
-  };
+  // Deleta todas as matriculas
+  app.delete("/matricula", matricula.deleteAll);
 };
--- server.js
+++ server.js
@@ -7,13 +7,20 @@ const app = express();
 app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({
+    message: "API do grupo."
+  });
 });
 require("./app/routes/aluno.routes.js")(app);
+require("./app/routes/resultado.routes.js")(app);
+require("./app/routes/disciplina.routes.js")(app);
+require("./app/routes/matricula.routes.js")(app);
 
 // set port, listen for requests
 app.listen(4000, () => {

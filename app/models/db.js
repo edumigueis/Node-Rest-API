4373be30354126016e1e9f8c1679ac7c430d29f8
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-03 19:33:38 -0300

    Ajustes

--- app/models/db.js
+++ app/models/db.js
@@ -1,18 +1,8 @@
-const mysql = require("mysql");
+
 const dbConfig = require("../config/db.config.js");
+const mssql = require('mssql');
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
-
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
-
-module.exports = connection;
+mssql.connect(dbConfig) 
+.then(conexao => global.conexao = conexao) 
+.catch(erro => console.log(erro));
+ module.exports = mssql;

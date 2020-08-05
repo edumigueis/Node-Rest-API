2f48c9bd450be30dbc215254049d8d6619f2cea0
digo-smithh <smithrodrigues08@gmail.com>
2020-08-05 13:03:59 -0300

    edit

---
+++ sintaxe.sql
@@ -0,0 +1,30 @@
+CREATE TABLE AlunosED (			
+RA SMALLINT PRIMARY KEY,		
+Nome VARCHAR(40) NOT NULL)		
+
+
+CREATE TABLE Disciplinas (
+Cod INT PRIMARY KEY,
+Nome VARCHAR(40) NOT NULL)
+
+CREATE TABLE Resultados (
+RA SMALLINT,
+Cod INT,
+Nota FLOAT NOT NULL,
+Frequencia FLOAT NOT NULL,
+PRIMARY KEY (RA,Cod),
+FOREIGN KEY (RA)
+REFERENCES AlunosED(RA),	
+FOREIGN KEY (Cod)							
+REFERENCES Disciplinas(Cod))
+
+CREATE TABLE MatriculasED (		
+RA SMALLINT,					
+Cod INT,						
+PRIMARY KEY (RA,Cod),			
+FOREIGN KEY (RA)				
+REFERENCES AlunosED(RA),		
+FOREIGN KEY (Cod)				
+REFERENCES Disciplinas(Cod))		
+
+						
